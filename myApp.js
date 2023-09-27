809c8d4559f226d369163671c9ac7ef55cade97c
shawn220a <shawn.42496@gmail.com>
2023-09-27 00:43:26 -0500

    Found missing code

--- myApp.js
+++ myApp.js
@@ -3,15 +3,15 @@ const helmet = require('helmet');
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
-
-// Use Helmet!
-app.use(helmet());
-
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
-
-app.listen(PORT, () => {
-  console.log(`server started on PORT ${PORT}`);
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
