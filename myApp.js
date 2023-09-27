ceb06d5335c1c5838ee29341dbe718a4efe14147
shawn220a <shawn.42496@gmail.com>
2023-09-27 00:08:21 -0500

    add helmet

--- myApp.js
+++ myApp.js
@@ -2,3 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 
 const app = express();
+
+// Use Helmet!
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(8000);
